43e2223214350780b6caf92782bd2cfd28cf0cee
Pierre Chalamet <pierre@magnusopera.io>
2023-11-03 20:35:43 +0100

    add unit test for empty lines

---
+++ PresqueYaml.Tests/Wellformed.fs
@@ -0,0 +1,39 @@
+module PresqueYaml.Wellformed.Tests
+
+open PresqueYaml
+open NUnit.Framework
+open FsUnit
+
+// ####################################################################################################################
+
+[<Test>]
+let ``empty lines are ignored``() =
+    let expected =
+        YamlNode.Mapping (Map [ "user1", YamlNode.Mapping (Map [ "name", YamlNode.Scalar "John Doe"
+                                                                 "age", YamlNode.None ] )
+                                "user2", YamlNode.Mapping (Map [ "name", YamlNode.Scalar "Jane Doe"
+                                                                 "age", YamlNode.Scalar "42" ] ) ])
+
+    let yaml = "
+    
+    
+user1:
+             
+  name: John Doe
+
+  age:
+
+                 
+user2:
+  name: Jane Doe
+                
+  age: 42
+  
+
+
+"
+
+    yaml
+    |> parse
+    |> should equal expected
+
