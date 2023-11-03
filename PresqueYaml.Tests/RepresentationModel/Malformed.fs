992172f43323cba4d9ca49bc7654d8ec75ce8d2f
Pierre Chalamet <pierre@magnusopera.io>
2023-11-03 23:28:43 +0100

    check sequence alignment in mapping

--- PresqueYaml.Tests/RepresentationModel/Malformed.fs
+++ PresqueYaml.Tests/RepresentationModel/Malformed.fs
@@ -49,3 +49,21 @@ let ``mapping value override must be on same line``() =
 
     (fun () -> yaml |> read |> ignore)
     |> should (throwWithMessage "Unexpected data type (line 3)") typeof<System.Exception>
+
+// ####################################################################################################################
+
+[<Test>]
+let ``sequence parent aligned in mapping is not valid``() =
+    let expected =
+        YamlNode.Mapping (Map [ "name", YamlNode.Scalar "John Doe"
+                                "age", YamlNode.Scalar "42"
+                                "languages", YamlNode.Sequence ["F#"; "Python" ] ])
+
+    let yaml = "name: John Doe
+age: 42
+languages:
+- F#
+- Python"
+
+    (fun () -> yaml |> read |> ignore)
+    |> should (throwWithMessage "Unexpected data type (line 4)") typeof<System.Exception>
