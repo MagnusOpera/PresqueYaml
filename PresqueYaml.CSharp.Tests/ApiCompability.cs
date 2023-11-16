2076cbd8ffc641214465d35b5178f271f954a743
Pierre Chalamet <pierre@chalamet.net>
2023-11-16 15:11:11 +0100

    remove warnings

--- PresqueYaml.CSharp.Tests/ApiCompability.cs
+++ PresqueYaml.CSharp.Tests/ApiCompability.cs
@@ -7,7 +7,7 @@ public class TestCSharpCompability
         var yaml = "item1: value1";
         var node = YamlParser.Read(yaml);
         var dic = YamlSerializer.Deserialize<Dictionary<string, string>>(node, Defaults.options);
-        Assert.AreEqual(1, dic.Count);
-        Assert.AreEqual(dic["item1"], "value1");
+        Assert.That(dic.Count, Is.EqualTo(1));
+        Assert.That(dic["item1"], Is.EqualTo("value1"));
     }
 }
